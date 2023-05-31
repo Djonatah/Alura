a7b5dd63a3bd2ff73b424aa5b2eff20aa029db59
Djonatah <djonatah@gmail.com>
2023-05-31 08:26:08 -0300

    added thymeleaf templates to the app

--- src/main/java/com/alura/djonatah/ScreenMatch/Controller/MovieController.java
+++ src/main/java/com/alura/djonatah/ScreenMatch/Controller/MovieController.java
@@ -2,6 +2,7 @@ package com.alura.djonatah.ScreenMatch.Controller;
 
 import com.alura.djonatah.ScreenMatch.Model.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +15,20 @@ import java.util.List;
 public class MovieController {
 
     private List<Movie> movies = new ArrayList<>();
-    @GetMapping
+    @GetMapping("form")
     public String loadForm(){
         return "movie/form.html";
     }
 
-    @PostMapping
-    public String saveForm(Movie movie){
-        movies.add(movie);
-        System.out.println(movies);
-        return "movie/form.html";
+    @GetMapping
+    public String list(Model model){
+        model.addAttribute("list",movies);
+        return "movie/list.html";
     }
 
+    @PostMapping
+    public String save(Movie movie){
+        movies.add(movie);
+        return "redirect:/movies";
+    }
 }
